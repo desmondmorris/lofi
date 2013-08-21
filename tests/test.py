3d47dbdbc44ca4f1d4b5dc1a81765673d3104ffb
Desmond Morris <hi@desmondmorris.com>
2013-08-21 15:18:47 -0400

    Adds logentries config

---
+++ tests/test.py
@@ -0,0 +1,17 @@
+from nose.tools import *
+import lofi
+
+def setup():
+    print "SETUP!"
+
+def teardown():
+    print "TEAR DOWN!"
+
+def test_basic():
+    print "I RAN!"
+
+def test_hard():
+    print "I RAN!"
+
+def test_medium():
+    print "I RAN!"
