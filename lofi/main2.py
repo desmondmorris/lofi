f46003538bc2509fdc53e1055ac0ed7434bc7028
Desmond Morris <hi@desmondmorris.com>
2013-08-22 15:23:30 -0400

    Lets try with tutorial code

--- lofi/main2.py
+++ lofi/main2.py
@@ -1,10 +1,11 @@
-from flask import Flask, request
-import json, os
-
+from flask import Flask
 app = Flask(__name__)
 
-if __name__ == '__main__':
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
     app.run(
-        debug=True,
         port=int(os.environ.get('PORT', 33507))
     )
