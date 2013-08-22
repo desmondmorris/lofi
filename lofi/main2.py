54510ff2606ffe57858e9f458b736f56ffd05274
Desmond Morris <hi@desmondmorris.com>
2013-08-22 15:12:27 -0400

    Lets try this again

--- lofi/main2.py
+++ lofi/main2.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(
-        host=os.environ['HOST'],
-        port=int(os.environ['PORT'])
+        debug=True,
+        port=int(os.environ.get('PORT', 33507))
     )
